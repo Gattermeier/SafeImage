e11f4c917ccb4ffa9ed2640f8263c796e7c8fbfe
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2016-11-23 13:19:52 -0500

    update example for github project pages demo

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,65 @@
 //import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from '../../lib'
+import {
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Grid, Row, Col
+  } from 'react-bootstrap'
+import SafeImage from '../../lib'
 import styles from './styles.css'
+
+
+const customStyles = {
+  col: {
+    border: '1px solid #dadada',
+    borderRadius: '3px',
+    padding: '.5em',
+    margin: '.5em'
+  },
+  example: {
+    padding: '2px',
+    borderRadius: '3px',
+    border: '1px dashed #5834ea'
+  }
+}
 
 const demo = (
   <div>
-    <Component><div>Child</div></Component>
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#"> SafeImage </a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="https://github.com/Gattermeier/SafeImage">Github</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <Grid>
+      <Row>
+        <Col sm={6} md={3} >
+          <div style={customStyles.col}>
+            <p>Rendered Image with custom styles prop</p>
+            <SafeImage className="img-responsive" src='http://lorempixel.com/400/200/' style={customStyles.example}/>
+          </div>
+        </Col>
+        <Col sm={6} md={3}>
+          <div style={customStyles.col}>
+            <p>Image with failing src props and alternative child components</p>
+            <SafeImage src='something.png'><div>Alternative Rendering</div></SafeImage>
+          </div>
+        </Col>
+        <Col sm={6} md={3}>
+          <div style={customStyles.col}>
+            <p>Image with failing src props and custom onError fn (see console logs)</p>
+            <SafeImage src='something.png' onError={() => console.log('custom onError handler triggered')} />
+          </div>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 ReactDOM.render(
