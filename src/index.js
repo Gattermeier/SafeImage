5b5bc3477ece761b934133b2b955cf3a45fa336a
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2016-11-23 12:04:50 -0500

    complete component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import ReactDOM from 'react-dom';
 export default class SafeImage extends Component {
   constructor(props) {
     super(props)
+    this.state = { error: false }
+  }
+  componentDidMount() {
+    const image = this.refs.image;
+
+    image.addEventListener('error', (e) => {
+      this.setState({ error: true })
+      this.props.onError && this.props.onError(e);
+    });
+
   }
   render() {
-    return (
-      <div>{this.props.children}</div>
-    )
+    const { src, children, style, onError, ...rest } = this.props
+    const { error } = this.state
+    if (src && !error) return <img src={src} style={style} ref='image' {...rest} />
+    if (error && children) return (<div>{children}</div>)
+    return (<div></div>)
   }
 }
